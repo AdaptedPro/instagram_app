0495e812d1baed884d1f7db03e2779cb05c2fe09
ajames <adamjames_pro@yahoo.com>
2013-07-01 08:05:02 -0700

    Access token in place
    Returning json reply and prepping for display

--- application/controllers/SearchController.php
+++ application/controllers/SearchController.php
@@ -1,8 +1,6 @@
 <?php
 class SearchController extends Zend_Controller_Action
 {
-
-	protected $_instagram = array();
 	
     public function init()
     {
@@ -11,60 +9,72 @@ class SearchController extends Zend_Controller_Action
     	$this->view->client_secret	= $config['instagram']['client_secret'];
     	$this->view->website_url	= $config['instagram']['website_url'];
     	$this->view->redirect_uri	= $config['instagram']['redirect_uri'];
+    	
+    	#SEND POST TO OBTAIN ACCESS TOKEN
+    	$ch		= curl_init();
+    	 
+    	$url	= 'https://api.instagram.com/oauth/access_token?"';
+    	$vars	= "client_id=". $this->view->client_id .
+    	"&client_secret=".$this->view->client_secret.
+    	"&redirect_uri=".$this->view->redirect_uri.
+    	"&grant_type=authorization_code".
+    	"&code=".$_GET['code'];
+    	 
+    	$ch = curl_init( $url );
+    	curl_setopt( $ch, CURLOPT_POST, 1);
+    	curl_setopt( $ch, CURLOPT_POSTFIELDS, $vars);
+    	curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+    	curl_setopt( $ch, CURLOPT_HEADER, 0);
+    	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+    	 
+    	$response = curl_exec( $ch );
+    	$response_json = json_decode($response);
+    	 
+    	#SET ACCESS TOKEN FOR CONTROLLER->VIEW
+    	$this->view->access_token = $response_json->access_token;   	
     }
     
     public function indexAction()
     {
     	#SET DB CONNECTIONS
     	#DECLARE VIEW VARS
+    	
+    	#SET VIEW
+    	$this->view->render("search/index.phtml");
     }
     
     public function startAction()
-    {
-    	$this->_helper->viewRenderer->setNoRender();
-    	$this->_helper->getHelper('layout')->disableLayout();
-    	
-    	$ch = curl_init();
-    	/*
-    	//THIS DOESN'T WORK EITHER
-    	$ch = curl_init("https://api.instagram.com/oauth/access_token?client_id=". $this->view->client_id .
-    			"&client_secret=".$this->view->client_secret.
-    			"&redirect_uri=".$this->view->redirect_uri.
-    			"&grant_type=authorization_code". 
-    			"&code=".$_GET['code']);
-    	*/
-    	//THIS DOESN'T WORK
-    	$data = array("client_id"	=> $this->view->client_id,
-    				"client_secret"	=> $this->view->client_secret,
-    				"grant_type"	=> "authorization_code",
-    				"redirect_uri"	=> $this->view->redirect_uri,
-    				"code"			=> $_GET['code']);
-    	
-    	curl_setopt($ch, CURLOPT_URL,"https://api.instagram.com/oauth/access_token");
-    	curl_setopt($ch, CURLOPT_POST, 1);
-    	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(data));    	
-    	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    	curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
-    	
-    	$server_output = curl_exec ($ch);
-    	curl_close ($ch);
-    	
-    	if ($server_output == "OK") {
-    		//CAN'T GET ACCESS TOKEN USING SUGGESTED STEPS
-    		echo "123";
-    	} else {
-    		echo "ABC";
-    	} 
+    {	
+    	#SET VIEW
+
+    	$this->view->token = $this->view->access_token;
+    	$_SESSION['access_token'] = $this->view->access_token;
+    	$this->view->render("search/start.phtml");    	
     }    
-    
-    public function ajaxsearchAction() 
+   						 
+   	public function ajaxsearchAction() 
     {
     	#DISABLE TEMPLATE FOR AJAX CALL
     	$this->_helper->viewRenderer->setNoRender();
     	$this->_helper->getHelper('layout')->disableLayout();
-    	    	  	 
-    	$json = file_get_contents($search_url);
-    	$json_data = json_decode($json);    	
+    	
+    	#GET SEARCH RESULTS
+    	$search_url	= "https://api.instagram.com/v1/locations/search?lat=".urlencode($_POST['cur_loc_lat'])."&lng=".urlencode($_POST['cur_loc_lon'])."&access_token=".urlencode($_POST['acc_token']);
+    	$json		= file_get_contents($search_url);
+    	$json_data	= json_decode($json, true);
+    	
+    	echo $this->buildOutput($json_data['data']);
+    }
+    
+    private function buildOutput($data)
+    {    	
+    	$output = "<ul> \n";
+    	foreach($data as $key => $val) {
+    		$output .= "<li>" .$val['name'] . "</li> \n";
+    	}
+    	$output .= "</ul> \n";
+    	
+    	return $output;
     }
 
 }
